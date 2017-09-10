a8864af8f69d87247f20845359f3a361e5d28810
Samer Abdel <samerros@gmail.com>
2017-09-10 13:56:02 +0200

    dalej to samo

--- breakTheHabit/DataManager.swift
+++ breakTheHabit/DataManager.swift
@@ -27,6 +27,13 @@ class DataManager {
     
     //  color, activityName, TimeSpentDoingIt, TimeLimit (if not bad habit TimeLimit = none || 0, Date)
     var activity: (color: UIColor, activityName: String, timeSpentDoingIt: Double, timeLimit: Double, date: Date)?
+    {
+        didSet{
+            addDateToArray(of: .day)
+            addDateToArray(of: .month)
+            //itp
+        }
+    }
 //
 //    struct Activity {
 //       static var color = UIColor()
@@ -43,21 +50,76 @@ class DataManager {
     
  //  Days = ["17.08":["sleeping":(red, "Sleeping", 12, 10)]]
     var arrayOfDays = [Date]()
-    var dayActivities = [String:(UIColor, String, Double, Double, Date)]()
+    var dayActivitiesName = [String]()
     var dayActivitiesColor = [UIColor]()
+    var dayActivitiesTime = [Double]()
     
     var arrayOfMonths = [Date]()
-    var monthActivities = [String:(UIColor, String, Double, Double, Date)]()
+    var monthActivities = [String:(String, Double, Double, Date)]()
     var monthActivitiesColor = [UIColor]()
     
     var arrayOfYears = [Date]()
-    var yearActivities = [String:(UIColor, String, Double, Double, Date)]()
+    var yearActivities = [String:(String, Double, Double, Date)]()
     var yearActivitiesColor = [UIColor]()
 
     
  //   var dictionary ("month": ["activity1": value1, "activity2": value2])
     
     
+    func addNameToArray(of: partsOfDate) {
+        
+        var partOfdate: Calendar.Component
+        var array = [Date]()
+        
+        
+        switch of {
+        case .day:
+            partOfdate = Calendar.Component.day
+            array = arrayOfDays
+        case .month:
+            partOfdate = Calendar.Component.month
+            array = arrayOfMonths
+        case .year:
+            partOfdate = Calendar.Component.year
+            array = arrayOfYears
+        }
+        
+        let previousDate: Date = arrayOfActivities[arrayOfActivities.endIndex - 2].date
+        let newDate: Date = arrayOfActivities.last!.date
+        
+        if calendar.component(partOfdate, from: previousDate) != calendar.component(partOfdate, from: newDate) {
+            array.append(newDate)
+        }
+        
+    }
+    
+    
+    func addColorToArray(of: partsOfDate) {
+        
+        var partOfdate: Calendar.Component
+        var array = [Date]()
+        
+        
+        switch of {
+        case .day:
+            partOfdate = Calendar.Component.day
+            array = arrayOfDays
+        case .month:
+            partOfdate = Calendar.Component.month
+            array = arrayOfMonths
+        case .year:
+            partOfdate = Calendar.Component.year
+            array = arrayOfYears
+        }
+        
+        let previousDate: Date = arrayOfActivities[arrayOfActivities.endIndex - 2].date
+        let newDate: Date = arrayOfActivities.last!.date
+        
+        if calendar.component(partOfdate, from: previousDate) != calendar.component(partOfdate, from: newDate) {
+            array.append(newDate)
+        }
+        
+    }
 
     
     func addDateToArray(of: partsOfDate) {
